14519ff83f79a095ef04c48a0d89a03fd24e9d14
skuralll <f20154@e.hiroshima-cu.ac.jp>
2024-09-29 18:07:31 +0900

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("jvm") version "1.9.22"
+    `java-gradle-plugin`
+}
+
+group = "com.skuralll"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+gradlePlugin{
+
+}
