d6ac5fea6994872b78586994d48b9b2d81338ca0
skuralll <f20154@e.hiroshima-cu.ac.jp>
2025-01-22 00:43:24 +0900

    バージョンアップ(1.0.7)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.kotlin.dsl.from
+
 plugins {
     kotlin("jvm") version "2.1.0"
     `java-gradle-plugin`
@@ -5,7 +7,7 @@ plugins {
 }
 
 group = "com.skuralll"
-version = "1.0.1"
+version = "1.0.7"
 
 repositories {
     mavenCentral()
@@ -26,12 +28,29 @@ kotlin {
     jvmToolchain(21)
 }
 
-
 gradlePlugin {
     plugins {
         create("mc_deploy") {
             id = "com.skuralll.mc_deploy"
             implementationClass = "com.skuralll.mc_deploy.MCDeployPlugin"
+        }
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "gpr"
+            url = uri("https://maven.pkg.github.com/skuralll/mc_deploy")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_USERNAME")
+                password = project.findProperty("gpr.token") as String? ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("gpr") {
+            from(components["java"])
         }
     }
 }
